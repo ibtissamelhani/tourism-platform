3ec859cad3fec6ea0cfd2cb9e11a3c61f8b669d9
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-02-10 12:08:21 +0100

    first commit

---
+++ src/main/java/org/ibtissam/dadesadventures/DadesAdventuresApplication.java
@@ -0,0 +1,13 @@
+package org.ibtissam.dadesadventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadesAdventuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadesAdventuresApplication.class, args);
+    }
+
+}
