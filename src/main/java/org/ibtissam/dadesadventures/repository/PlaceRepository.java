b6874f8d6747beb105c5cf74a8fef110116de357
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-02-15 14:31:47 +0100

    adding repositories

--- src/main/java/org/ibtissam/dadesadventures/repository/PlaceRepository.java
+++ src/main/java/org/ibtissam/dadesadventures/repository/PlaceRepository.java
@@ -1,9 +1,9 @@
 package org.ibtissam.dadesadventures.repository;
 
-import org.ibtissam.dadesadventures.domain.entities.Category;
+import org.ibtissam.dadesadventures.domain.entities.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CategoryRepository  extends JpaRepository<Category, UUID> {
+public interface PlaceRepository extends JpaRepository<Place, UUID> {
 }
