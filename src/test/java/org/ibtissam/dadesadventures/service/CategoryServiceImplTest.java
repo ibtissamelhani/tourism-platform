ccdd57b21cd34e8b50b724e7961447c3eae3808b
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-02-24 19:28:39 +0100

    test dependencies added

---
+++ src/test/java/org/ibtissam/dadesadventures/service/CategoryServiceImplTest.java
@@ -0,0 +1,31 @@
+package org.ibtissam.dadesadventures.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class CategoryServiceImplTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void searchByName() {
+    }
+}
