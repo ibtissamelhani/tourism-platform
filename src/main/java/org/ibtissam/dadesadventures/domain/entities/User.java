6afbc129d0c18cc4952ba5d24fbda083346647ba
ibtissam EL HANI <ibtissamelhani36@gmail.com>
2025-03-10 12:18:38 +0100

    quick fix token

--- src/main/java/org/ibtissam/dadesadventures/domain/entities/User.java
+++ src/main/java/org/ibtissam/dadesadventures/domain/entities/User.java
@@ -80,10 +80,12 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return role.getAuthorities();
     }
+
     @Override
     public String getUsername() {
         return email;
