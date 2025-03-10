a0f5e69f666cf11682c65b395211e837ebfc58d9
ibtissam EL HANI <ibtissamelhani36@gmail.com>
2025-03-10 11:55:25 +0100

    permissions added

---
+++ src/main/java/org/ibtissam/dadesadventures/domain/enums/Permission.java
@@ -0,0 +1,17 @@
+package org.ibtissam.dadesadventures.domain.enums;
+
+public enum Permission {
+
+    REGISTER,
+    VIEW_PLACES_ACTIVITIES,
+    LEAVE_REVIEW,
+    BOOK_ACTIVITY,
+
+    CREATE_ACTIVITY,
+    MODIFY_OWN_ACTIVITY,
+    CANCEL_OWN_ACTIVITY,
+
+    MANAGE_ACTIVITIES,
+    MANAGE_USERS,
+    MANAGE_PLACES_CATEGORIES
+}
